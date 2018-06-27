41e0fea866056bcd4cb157bd03032eb9a57884b4
Marchio Roque M <martinmarchio@hotmail.com>
2018-06-27 12:49:49 -0300

    Se blanque proyecto front end por mal funcionamiento

---
+++ BackEnd/Scripts.sql
@@ -0,0 +1,19 @@
+﻿--TABLAS---------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `trabajos` (
+  `id` INTEGER PRIMARY KEY ASC,
+  `nombre` TEXT,
+  `title` TEXT,
+  `listing_title` TEXT,
+  `size` TEXT,
+  `queue_name` TEXT,
+  `local_server` TEXT,
+  `customer_name` TEXT,
+  `owner` TEXT,
+  `host_name` TEXT,
+  `device` TEXT,
+  `fecha` TEXT NOT NULL ,
+  `pages` TEXT,
+  `size_pdf` TEXT,
+  `status` TEXT
+)
